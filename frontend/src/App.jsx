4fa582ea74cded984e91005673256aa173773e77
i-manojyadav <i.manojyadav325@gmail.com>
2026-07-03 15:38:24 +0530

    feat: implement holdings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/layout/Header';
+import Holdings from './pages/holdings/Holdings';
 import Orders from './pages/orders/Orders';
 
 function App() {
   
   return (
     <>
-      <Orders />
+      <Header />
+      <Holdings />
     </>
   )
 }
---
+++ frontend/src/pages/holdings/Holdings.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react';
+import './Holdings.css';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, } from "@mui/material";
+
+function Holdings() {
+
+    const [ holdings, setHoldings ] = useState([
+        {
+            instrument: "HDFC Bank",
+            quantity: "55",
+            average: "45",
+            ltp: "800",
+            curValue: "790",
+        },
+    ]);
+
+    return (
+        <div className='holdings'>
+            <h3>Holdings</h3>
+            <TableContainer>
+                <Table className='holdings-table'>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Instrument</TableCell>
+                            <TableCell>Qty</TableCell>
+                            <TableCell>Avg. Cost</TableCell>
+                            <TableCell>LTP</TableCell>
+                            <TableCell>Cur. Val</TableCell>
+                            <TableCell>P&L</TableCell>
+                            <TableCell>Net Chg.</TableCell>
+                            <TableCell>Day Chg.</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {holdings.map((holding, idx) => (
+                            <TableRow key={idx}>
+                                <TableCell>{holding.instrument}</TableCell>
+                                <TableCell>{holding.quantity}</TableCell>
+                                <TableCell>{holding.average}</TableCell>
+                                <TableCell>{holding.ltp}</TableCell>
+                                <TableCell>{holding.curValue}</TableCell>
+                                <TableCell>{(Number(holding.ltp) - Number(holding.average)) * Number(holding.quantity)}</TableCell>
+                                <TableCell>{}</TableCell>
+                                <TableCell>{}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </div>
+    )
+}
+
+
+export default Holdings;
