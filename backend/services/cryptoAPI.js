0b59a0c21234ef85e04102930ab96bc16773257d
i-manojyadav <i.manojyadav325@gmail.com>
2026-08-15 22:21:12 +0530

    chore: remove debug logs

--- backend/services/cryptoAPI.js
+++ backend/services/cryptoAPI.js
@@ -3,9 +3,6 @@ const URL = "https://api.binance.com/api/v3/ticker/24hr";
 const CryptoData = async () => {
     const response = await fetch(URL);
     const data = await response.json();
-    console.log("STATUS:", response.status);
-    console.log("IS ARRAY",Array.isArray(data));
-    console.log("DATA", data);
     const USDTCoins = data?.filter((coin) => {
         return coin.symbol.endsWith("USDT");
     });
