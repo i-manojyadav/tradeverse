801ea958e855aeafd40385ac7695b3c045df3832
i-manojyadav <i.manojyadav325@gmail.com>
2026-08-18 14:19:26 +0530

    fix: crypto API context bug

--- frontend/src/context/CryptoAPIContext.jsx
+++ frontend/src/context/CryptoAPIContext.jsx
@@ -26,7 +26,7 @@ export default function CryptoAPIProvider({ children }) {
 
         return () => clearInterval(interval);
 
-    }, []);
+    }, [user]);
 
     return (
         <CryptoAPIContext.Provider
