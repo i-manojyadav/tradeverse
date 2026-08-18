ac2f40369d83ed29c11e2ba13941eaa32686d79a
i-manojyadav <i.manojyadav325@gmail.com>
2026-08-18 15:28:26 +0530

    style: improve watchlist UI

--- frontend/src/components/watchlist/Watchlist.jsx
+++ frontend/src/components/watchlist/Watchlist.jsx
@@ -6,6 +6,8 @@ import { WatchlistContext } from '../../context/WatchlistContext';
 import CryptoData from '../../services/cryptoAPI';
 import { CryptoAPIContext } from '../../context/CryptoAPIContext';
 import AppAlert from '../ui/AppAlert';
+import { AuthContext } from '../../context/AuthContext';
+import SignInPrompt from '../emptyStates/SignInPrompt';
 
 const url = import.meta.env.VITE_API_URL;
 
@@ -13,8 +15,11 @@ function Watchlist() {
 
     const inputRef = useRef();
 
+    const isMobile = window.innerWidth <= 768;
+
     const [ alert, setAlert ] = useState(null);
 
+    const { user } = useContext(AuthContext);
     const { watchlist, setWatchlist } = useContext(WatchlistContext);
     const { coins } = useContext(CryptoAPIContext);
 
@@ -206,7 +211,8 @@ function Watchlist() {
 
 
     return (
-        <div className='watchlist'>
+        <>
+        {user && <div className='watchlist'>
             { alert && <AppAlert msg={alert.msg} severity={alert.severity} /> }
 
             <div className='watchlist-search' style={{ display: searchSecActive ? "block" : "none"}}>
@@ -240,8 +246,8 @@ function Watchlist() {
                             <p className='watchlist-price'>
                                 <span style={{ color: "#ffffff"}}>{coin.symbol}</span>
                                 <span>
-                                    <span style={{ color: "#c4c4c4"}}>{Number(Number(coin.lastPrice).toFixed(1)).toLocaleString()}</span>
-                                    <span style={{ color: coin.priceChange > 0 ? "#008000" : "#ff0000"}}>{Number(Number(coin.priceChangePercent).toFixed(1)).toLocaleString()}%</span>
+                                    <span style={{ color: "#c4c4c4"}}>{Number(Number(coin.lastPrice).toFixed(2)).toLocaleString()}</span>
+                                    <span style={{ color: coin.priceChange > 0 ? "#008000" : "#ff0000"}}>{Number(Number(coin.priceChangePercent).toFixed(2)).toLocaleString()}%</span>
                                 </span>
                             </p>
                             <p className='watchlist-action'>
@@ -255,7 +261,9 @@ function Watchlist() {
                     <button onClick={() => searchSecToggle()} className='add-symbol-btn'><i className="fa-solid fa-plus"></i> Add Symbol</button>
                 </div>
             </div>
-        </div>
+        </div>}
+        {(isMobile && !user) && <SignInPrompt />}
+        </>
     )
 }
 
