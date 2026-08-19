b4a36f1a41bf38066dfb91090f6bbf98a992f91a
i-manojyadav <i.manojyadav325@gmail.com>
2026-08-19 15:31:21 +0530

    fix: handle invalid API response

--- backend/services/cryptoAPI.js
+++ backend/services/cryptoAPI.js
@@ -3,7 +3,7 @@ const URL = "https://api.binance.com/api/v3/ticker/24hr";
 const CryptoData = async () => {
     const response = await fetch(URL);
     const data = await response.json();
-    if (!data) return;
+    if (!Array.isArray(data)) return;
     const USDTCoins = data?.filter((coin) => {
         return coin.symbol.endsWith("USDT");
     });
