173e2d9a7e7e937e9ba1f8cc784f8c6700801196
i-manojyadav <i.manojyadav325@gmail.com>
2026-08-19 16:44:31 +0530

    config: set crypto API endpoint

--- backend/services/cryptoAPI.js
+++ backend/services/cryptoAPI.js
@@ -1,8 +1,15 @@
-const URL = "https://api.binance.com/api/v3/ticker/24hr";
+const URL = process.env.CRYPTO_API_URL;
 
 const CryptoData = async () => {
     const response = await fetch(URL);
     console.log("Crypto API Status:", response.status);
+
+    if (response.status === 418) {
+        const retryAfter = response.headers.get("Retry-After");
+        console.log("Retry After:", retryAfter);
+        return;
+    }
+
     const data = await response.json();
     if (!Array.isArray(data)) return;
     const USDTCoins = data?.filter((coin) => {
