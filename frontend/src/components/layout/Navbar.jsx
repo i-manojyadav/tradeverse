5620cf309e4eb03d9c4c9444c76e153e9000099c
i-manojyadav <i.manojyadav325@gmail.com>
2026-09-19 11:05:20 +0530

    fix: resolve navbar account popup issues

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -65,13 +65,13 @@ function Navbar() {
                 <p>Tradeverse</p>
             </div>
             <div className='navbar-right'>
-                <NavLink to='/' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i class="fa-solid fa-gauge-high"></i> <span>Dashboard</span></NavLink>
-                <span className='wList'><NavLink to='/watchlist' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i class="fa-solid fa-bookmark"></i> <span>Watchlist</span></NavLink></span>
-                <NavLink to='/chart' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i class="fa-solid fa-chart-line"></i> <span>Chart</span></NavLink>
-                <NavLink to='/orders' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i class="fa-solid fa-list-check"></i> <span>Orders</span></NavLink>
-                <NavLink to='/portfolio' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i class="fa-solid fa-suitcase"></i> <span>Portfolio</span></NavLink>
+                <NavLink onClick={() => setIsActive(false)} to='/' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i className="fa-solid fa-gauge-high"></i> <span>Dashboard</span></NavLink>
+                <span className='wList'><NavLink onClick={() => setIsActive(false)} to='/watchlist' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i className="fa-solid fa-bookmark"></i> <span>Watchlist</span></NavLink></span>
+                <NavLink onClick={() => setIsActive(false)} to='/chart' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i className="fa-solid fa-chart-line"></i> <span>Chart</span></NavLink>
+                <NavLink onClick={() => setIsActive(false)} to='/orders' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i className="fa-solid fa-list-check"></i> <span>Orders</span></NavLink>
+                <NavLink onClick={() => setIsActive(false)} to='/portfolio' className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}><i className="fa-solid fa-suitcase"></i> <span>Portfolio</span></NavLink>
                 <div>
-                    <NavLink onClick={handlePopup} className='sub-nav-link' ><i class="fa-solid fa-circle-user"></i> <span>Account</span></NavLink>
+                    <NavLink onClick={handlePopup} className='sub-nav-link' ><i className="fa-solid fa-circle-user"></i> <span>Account</span></NavLink>
                     <div style={{ display: isActive ? "inline-block" : "none"}} >
                         <div className='ac-popup'>
                             {user && <NavLink onClick={handlePopup} to='/account' className='sub-nav-link'>Account</NavLink>}
